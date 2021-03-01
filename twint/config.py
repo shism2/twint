83af6e529251afdd2489eff331af9b2232b12794
shism2 <jcastilla358@gmail.com>
2021-02-28 21:05:23 -0500

    Update config.py

--- twint/config.py
+++ twint/config.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 @dataclass
 class Config:
-     Worker_ID: Optional[str] = None
+    Worker_ID: Optional[str] = None
     Username: Optional[str] = None
     User_id: Optional[str] = None
     Search: Optional[str] = None
